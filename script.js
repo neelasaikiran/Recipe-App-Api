8f0c94fe845f20129092c70214f1838664ea71d3
NEELA SAIKIRAN <neelasaikiran123@gmail.com>
2024-08-16 21:06:03 +0530

    Adding styles commit

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ const recipeContainer = document.querySelector(".recipe-container");
 
 // function to get recipes
 const fetchRecipes = async (query) => {
+    recipeContainer.innerHTML = '<h2>Fetching Recipes...</h2>';
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response = await data.json();
 
-    response.meals.forEach(meal =>{
-     const recipeDiv = document.createElement('div');
-     recipeDiv.classList.add('recipe');
-     recipeDiv.innerHTML = `
+    recipeContainer.innerHTML = '';
+    response.meals.forEach(meal => {
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML = `
      <img src="${meal.strMealThumb}">
      <h3>${meal.strMeal}</h3>
      <p> ${meal.strArea} <p>
      <p> ${meal.strCategory} <p>
 
 
-     `  
-     recipeContainer.appendChild(recipeDiv);
+     `
+        recipeContainer.appendChild(recipeDiv);
     })
     // console.log(response);
 }
@@ -27,5 +29,5 @@ searchBtn.addEventListener('click', (e) => {
     e.preventDefault(); // if refresh the page the data will be constant
     const searchInput = searchBox.value.trim()
     fetchRecipes(searchInput);
-    
+
 })
