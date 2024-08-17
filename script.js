3e7db4acc3de50b2d3d50257f7188b8cbef8f224
NEELA SAIKIRAN <neelasaikiran123@gmail.com>
2024-08-17 13:42:32 +0530

    Added some more styles

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const searchBox = document.querySelector(".searchbox");
 const searchBtn = document.querySelector(".searchBtn");
 const recipeContainer = document.querySelector(".recipe-container");
+const recipeDetailsContent = document.querySelector(".recipe-details-content");
+const recipeCloseButton = document.querySelector(".recipe-close-btn");
 
 // function to get recipes
 const fetchRecipes = async (query) => {
@@ -15,16 +17,31 @@ const fetchRecipes = async (query) => {
         recipeDiv.innerHTML = `
      <img src="${meal.strMealThumb}">
      <h3>${meal.strMeal}</h3>
-     <p> ${meal.strArea} <p>
-     <p> ${meal.strCategory} <p>
-
-
+     <p>  <span> ${meal.strArea} </span> Dish<p>
+     <p>Belongs to <span>  ${meal.strCategory} </span> category <p>
+     
      `
+        const button = document.createElement('button');
+        button.textContent = "View Recipe";
+        recipeDiv.appendChild(button);
+
+
+        // Adding EventListener 
+
+        button.addEventListener('click', () => {
+            openRecipePopup(meal);
+        })
         recipeContainer.appendChild(recipeDiv);
     })
     // console.log(response);
 }
-
+const openRecipePopup = (meal) =>{
+    recipeDetailsContent.textContent = 
+    `
+    <h2>${meal.strMeal}</h2>
+    `
+    recipeDetailsContent.parentElement.style.display = "block";
+}
 searchBtn.addEventListener('click', (e) => {
     e.preventDefault(); // if refresh the page the data will be constant
     const searchInput = searchBox.value.trim()
